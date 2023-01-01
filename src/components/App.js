5e812e29418c89bd90b14dfe0420a9174fcf5a6f
{sa0113ki} <{sa9113ki@gmail.com}>
2023-01-01 14:17:39 +0900

    commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+// import React from 'react';
+// import Image from "./Image"
+
+// function App(){
+//   return(
+//     <div>
+//       <Image/>
+//     </div>
+//   )
+// };
+
+//  export default App;
+
+  
---
+++ src/components/Image.js
@@ -0,0 +1,60 @@
+// const Image = () => {
+//     return <div>
+//         <img src= "" id="d1" className="card"></img>
+//         <img src= "" id="d2" className="card"></img>
+//         <img src= "" id="d3" className="card"></img>
+//         <img src= "" id="d4" className="card"></img>
+//         </div>
+//       };
+// export default Image;
+
+
+
+// function rand(max){
+//     return Math.floor(Math.random()*(max+1));
+// }
+
+// let cards = [
+//     {img :'c1.png', Number:1},{img :'c2.png', Number:2},{img :'c3.png', Number:3},{img :'c4.png', Number:4},
+//     {img :'c5.png', Number:5},{img :'c6.png', Number:6},{img :'c7.png', Number:7},{img :'c8.png', Number:8},{img :'c9.png', Number:9},
+//     {img :'c10.png', Number:10},{img :'c11.png', Number:11},{img :'c12.png', Number:12},{img :'c13.png', Number:13},
+//     {img :'d1.png', Number:1},{img :'d2.png', Number:2},{img :'d3.png', Number:3},{img :'d4.png', Number:4},
+//     {img :'d5.png', Number:5},{img :'d6.png', Number:6},{img :'d7.png', Number:7},{img :'d8.png', Number:8},{img :'d9.png', Number:9},
+//     {img :'d10.png', Number:10},{img :'d11.png', Number:11},{img :'d12.png', Number:12},{img :'d13.png', Number:13},
+//     {img :'h1.png', Number:1},{img :'h2.png', Number:2},{img :'h3.png', Number:3},{img :'h4.png', Number:4},
+//     {img :'h5.png', Number:5},{img :'h6.png', Number:6},{img :'h7.png', Number:7},{img :'h8.png', Number:8},{img :'h9.png', Number:9},
+//     {img :'h10.png', Number:10},{img :'h11.png', Number:11},{img :'h12.png', Number:12},{img :'h13.png', Number:13},
+//     {img :'s1.png', Number:1},{img :'s2.png', Number:2},{img :'s3.png', Number:3},{img :'s4.png', Number:4},
+//     {img :'s5.png', Number:5},{img :'s6.png', Number:6},{img :'s7.png', Number:7},{img :'s8.png', Number:8},{img :'s9.png', Number:9},
+//     {img :'s10.png', Number:10},{img :'s11.png', Number:11},{img :'s12.png', Number:12},{img :'s13.png', Number:13}
+// ];
+
+// for (let i = cards.length-1 ; i>0; i--)
+// {
+//     let r = rand(i);
+//     let tmp = cards[i];
+//     cards[i] = cards[r];
+//     cards[r] = tmp;
+
+// }
+// // player用デッキ
+// let deck1 = [cards[0],cards[1],cards[2],cards[3],cards[4],cards[5],cards[6],cards[7],cards[8],cards[9],
+// cards[10],cards[11],cards[12],cards[13],cards[14],cards[15],cards[16],cards[17],cards[18],cards[19],
+// cards[20],cards[21],cards[22],cards[23],cards[24],cards[25]]
+
+// // dealer用デッキ
+// let deck2 = [cards[26],cards[27],cards[28],cards[29],cards[30],cards[31],cards[32],cards[33],cards[34],cards[35],cards[36],
+// cards[37],cards[38],cards[39],cards[40],cards[41],cards[42],cards[43],cards[44],cards[45],cards[46],
+// cards[47],cards[48],cards[49],cards[50],cards[51]]
+
+// // 最初0〜3を
+// d1 = deck1[0]
+// d2 = deck1[1]
+// d3 = deck1[2]
+// d4 = deck1[3]
+// // nextd = 最後に配れた　＋1
+// nextd > deck1.length ... {空}
+
+
+
+
